7cb18e520532ac1657fa186bf297d7a7a19c57f4
nirglus <nirgluskin@gmail.com>
2024-08-08 08:42:20 +0300

    adjust url

--- Server/utils/fetchWeather.js
+++ Server/utils/fetchWeather.js
@@ -3,7 +3,7 @@ const { NotFoundError } = require('../errors/customErrors');
 
 const fetchWeather = async (city, apiKey) => {
     const baseURL = process.env.API_BASE_URL;
-    const url = `${baseURL}/current.json?key=${apiKey}&q=${city}`;
+    const url = `${baseURL}/forecast.json?key=${apiKey}&q=${city}`;
     try {
         const res = await axios.get(url);
         return res.data;
